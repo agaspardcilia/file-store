09675ab83283581b01e3caa3921c9f17cd7fe356
Alexandre Gaspard Cilia <a.gaspardcilia@gmail.com>
2024-01-21 19:41:17 +0000

    generate base with spring initializr

---
+++ src/test/java/fr/agaspardcilia/filestore/FileStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.agaspardcilia.filestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
